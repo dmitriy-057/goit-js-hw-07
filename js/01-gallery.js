6160a8dc050054022e34696c3ee1c7455d48b4d0
Dmitriy Goncharov <my.webpage057@gmail.com>
2022-07-17 01:00:14 +0300

    add SimpleLightbox task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,16 +1,8 @@
-// Выполняй это задание в файлах 01-gallery.html и 01-gallery.js. Разбей его на несколько подзадач:
-
-// Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
-// Реализация делегирования на div.gallery и получение url большого изображения.
-// Подключение скрипта и стилей библиотеки модального окна basicLightbox. Используй CDN сервис jsdelivr и добавь в проект ссылки на минифицированные (.min) файлы библиотеки.
-// Открытие модального окна по клику на элементе галереи. Для этого ознакомься с документацией и примерами.
-// Замена значения атрибута src элемента <img> в модальном окне перед открытием. Используй готовую разметку модального окна с изображением из примеров библиотеки basicLightbox.
 import { galleryItems } from "./gallery-items.js";
 
-// Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
 const galleryContainer = document.querySelector(".gallery");
 console.log(galleryContainer);
-// const cardMarkup = createImagesCardsMarkup(galleryItems);
+
 galleryContainer.insertAdjacentHTML(
   "beforeend",
   createImagesCardsMarkup(galleryItems)
@@ -19,32 +11,49 @@ function createImagesCardsMarkup(galleryItems) {
   return galleryItems
     .map(({ preview, original, description }) => {
       return `
-    
-        <a class="gallery__item" href=${original}>
-            <img 
-                class="gallery__image" src=${preview}
-                data-source=${original}
-                alt=${description} 
-                
-            />
-        </a>
+    <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
        `;
     })
     .join("");
 }
-// Реализация делегирования на div.gallery и получение url большого изображения.
+
 galleryContainer.addEventListener("click", onGalleryContainerClick);
 
+// Реализация делегирования на div.gallery и получение url большого изображения.
+
 function onGalleryContainerClick(e) {
-  e.target.preventDefault();
+  e.preventDefault();
   const IsGalleryImage = e.target.classList.contains("gallery__image");
+  // указываем цель клику
   if (!IsGalleryImage) {
     return;
   }
-  e.target.dataset.source;
-  // Подключение скрипта и стилей библиотеки модального окна basicLightbox. Используй CDN сервис jsdelivr и добавь в проект ссылки на минифицированные (.min) файлы библиотеки.
+
   const instance = basicLightbox.create(
-    `<img data-source=${original} width="800" height="600"/>`
+    `<img src='${e.target.dataset.source}'/>`
   );
-  instance.show();
+  instance.show(() => console.log("lightbox now visible"));
 }
+
+// вариант с методом .nodeName
+// function onGalleryContainerClick(e) {
+//   e.preventDefault();
+
+//   if (e.target.nodeName !== "IMG") {
+//     return;
+//   }
+
+//   const instance = basicLightbox.create(
+//     `<img src='${e.target.dataset.source}'/>`
+//   );
+//   instance.show(() => console.log("lightbox now visible"));
+// }
